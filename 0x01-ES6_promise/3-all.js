a2b74afbc46af38b641e797f8543692e8fbc0472
Pop2323 <omar4500455@gmail.com>
2024-04-11 00:55:25 -0700

    update

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,15 @@
-import { uploadPhoto, createUser } from './utils';
+import { uploadPhoto, createUser } from "./utils.js";
 
-export default function handleProfileSignup() {
-    return Promise.all([uploadPhoto(), createUser()])
-    .then((response) => {
-        console.log(`${response[0].body} ${response[1].firstName} ${response[2].lastName}`);
-    })
-    .catch(() => console.log("Signup system offline"));
-};
+async function handleProfileSignup() {
+    try {
+        const [photoResponse, userResponse] = await Promise.all([
+            uploadPhoto(),
+            createUser()
+        ]);
+        console.log(userResponse.body.firstName, userResponse.body.lastName);
+    } catch (error) {
+        console.error("Signup system offline");
+    }
+}
+
+export default handleProfileSignup;
