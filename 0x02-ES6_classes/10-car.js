b76782958a858584ce77cc533208ede713a7f87e
Pop2323 <omar4500455@gmail.com>
2024-04-11 12:10:05 -0700

    update1

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,35 +1,11 @@
 export default class Car {
-    constructor(brand, motor, color) {
-        this._brand = brand;
-        this._motor = motor;
-        this._color = color;
-    }
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
 
-    get brand() {
-        return this._brand
-    }
-
-    set brand(val) {
-        this._brand = val;
-    }
-
-    get motor() {
-        return this._motor;
-    }
-
-    set motor(val) {
-        this._motor = val;
-    }
-
-    get color() {
-        return this._color;
-    }
-
-    set color(val) {
-        this._color = val;
-    }
-
-    static cloneCar(car) {
-        return new Car(car.brand, car.motor, car.color);
-    }
+  cloneCar() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
 }
