478dbc07047d946d614c67cdd27cf5540042accb
Pop2323 <omar4500455@gmail.com>
2024-04-11 01:43:27 -0700

    update2

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,13 @@
-import signUpUser from "./4-user-promise.js";
-import uploadPhoto from "./5-photo-reject.js";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
-    return Promise.allSettled([
-        signUpUser(firstName, lastName),
-        uploadPhoto(filename)
-    ]).then((res) => {
-        const arr = [];
-        for (const i of res) {
-            arr.push({ status: i.status, value: i.value || i.reason });
-        }
-        return arr;
-    });
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => (
+      results.map((out) => ({
+        status: out.status,
+        value: out.status === 'fulfilled' ? out.value : String(out.reason),
+      }))
+    ));
 }
