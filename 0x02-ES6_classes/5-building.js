ce746fcdeca3eaa8b90bd877f1aa8490536c8437
Pop2323 <omar4500455@gmail.com>
2024-04-11 11:53:24 -0700

    update1

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,12 @@
 export default class Building {
-    constructor(sqft) {
-        if (typeof sqft !== 'number') throw new TypeError('Class extending Building must override evacuationWarningMessage');
-        this._sqft = sqft;
+  constructor(sqft) {
+    if (new.target !== Building) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
+    this._sqft = sqft;
+  }
 
-    get sqft() {
-        return this._sqft;
-    }
+  get sqft() {
+    return this._sqft;
+  }
 }
