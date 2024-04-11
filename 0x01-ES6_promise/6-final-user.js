61b8770b96e1827b92c13d73770bca48f9371f7b
Pop2323 <omar4500455@gmail.com>
2024-04-10 20:00:20 -0700

    first commit

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signUpUser from 4-user-promise.js;
+import uploadPhoto  from 5-photo-reject.js;
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(filename)
+]).then((res) => {
+    const arr = [];
+    for (const i of res) {
+        arr.push(status: i.status, value: i.value || i.reason)
+    }
+    return arr;
+});
+}
