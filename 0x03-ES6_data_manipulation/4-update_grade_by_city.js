3ae78c29722f8d85f7d97a0cda1cffeda2847366
Pop2323 <omar4500455@gmail.com>
2024-04-16 09:34:33 -0700

    first commit

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,9 @@
+export default function updateStudentGradeByCity(arry, city, newGrades) {
+	return arry
+		.filter((student) => student.location === city)
+		.map((student) => {
+			const grades = newGrades.filter(grade => grade.studentId === student.id);
+			const grade = grades.length ? grades[0].grade : 'N/A';
+			return {...student, grade};
+		});
+}
---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(set, startString) {
+	let res = '';
+	if (!startString || startString.length === 0) {
+		return res;
+	}
+
+	for (const value of set) {
+		if (value && value.startsWith(startString)) {
+			res += `${value.slice(startString.length)}-`;
+		}
+	}
+
+	return res.slice(0, -1);
+}
