fe051fb430b6590e9298b463af8b11646af809f7
Pop2323 <omar4500455@gmail.com>
2024-04-16 10:57:48 -0700

    first commit

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,18 @@
-export default function createInt8TypedArray(length, position, value) {
-	if (position >= length) {
-		throw new Error('Position outside range');
-	}
+export default function createInt8TypedArray(
+  length,
+  position,
+  value,
+) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
 
-	const buffer = new DataView(
-		new ArrayBuffer(length), 0, length,
-	);
-	buffer.setUint8(position, value);
-	return buffer;
+  const buffer = new DataView(
+    new ArrayBuffer(length),
+    0,
+    length,
+  );
+
+  buffer.setUint8(position, value);
+  return buffer;
 }
