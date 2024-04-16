1713620e544ff8ce4a27e76977267c2599838107
Pop2323 <omar4500455@gmail.com>
2024-04-16 11:10:53 -0700

    first commit

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,10 @@
 export default function cleanSet(set, startString) {
-	let res = '';
-	if (!startString || startString.length === 0) {
-		return res;
-	}
-
-	for (const value of set) {
-		if (value && value.startsWith(startString)) {
-			res += `${value.slice(startString.length)}-`;
-		}
-	}
-
-	return res.slice(0, -1);
+  let res = '';
+  if (!startString || startString.length === 0) return res;
+  for (const value of set) {
+    if (value && value.startsWith(startString)) {
+      res += `${value.slice(startString.length)}-`;
+    }
+  }
+  return res.slice(0, -1);
 }
