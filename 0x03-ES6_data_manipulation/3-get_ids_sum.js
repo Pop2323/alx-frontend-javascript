013a9a55218fc846370ece6e64d02dae0e47c25e
Pop2323 <omar4500455@gmail.com>
2024-04-16 10:29:27 -0700

    first commit

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,5 @@
 import getListStudents from './0-get_list_students';
 
-export default function getStudentIdsSum(std = getListStudents()) {
+export default function getStudentIdsSum(student = getListStudents()) {
    return std.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0);
 }
