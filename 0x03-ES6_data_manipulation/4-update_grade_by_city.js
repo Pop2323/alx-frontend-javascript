5acbea61fb1b038a7524b310950cd5eb9d03c97f
Pop2323 <omar4500455@gmail.com>
2024-04-16 10:53:58 -0700

    first commit

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,10 @@
 export default function updateStudentGradeByCity(arr, city, newGrades) {
   return arr
-    .filter((std) => std.location === city)
-    .map((student) => {
-      const grades = newGrades.filter((std) => std.studentId === student.id);
+    .filter((el) => el.location === city)
+    .map((person) => {
+      const grades = newGrades.filter((el) => el.studentId === student.id);
       const grade = grades.length ? grades[0].grade : 'N/A';
-       return {
+      return {
         ...student,
         grade,
       };
