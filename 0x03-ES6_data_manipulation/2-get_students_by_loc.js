aaf09176d755b486d169aed683f8ac5f85ff169b
Pop2323 <omar4500455@gmail.com>
2024-04-16 10:22:29 -0700

    first commit

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,5 @@
 import getListStudents from './0-get_list_students';
 
 export default function getStudentsByLocation(std = getListStudents(), city) {
-   return std.filter((el) => el.location === city);
+  return std.filter((el) => el.location === city);
 }
